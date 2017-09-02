1bbb9b2490adb504dd5dfbe92c6d65947ef26d74
madwrist97 <maurojose1997@gmail.com>
2017-09-02 18:24:11 -0400

    Message Displayed
    Volvi a cambiarlo! ^.^

--- Learning/src/learning/Learning.java
+++ Learning/src/learning/Learning.java
@@ -22,12 +22,12 @@ public class Learning extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Ya puedoo");
+        btn.setText("Clickeame");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Gracias!");
             }
         });
         
