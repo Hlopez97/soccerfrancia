95213e2a8045f721f1e16579ce1f9da5cc2280c2
LucasPradel <31575988+LucasPradel@users.noreply.github.com>
2017-09-02 17:59:03 -0400

    ola gui

--- Learning/src/learning/Learning.java
+++ Learning/src/learning/Learning.java
@@ -22,7 +22,7 @@ public class Learning extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello pussy'");
+        btn.setText("Say ola gui'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
