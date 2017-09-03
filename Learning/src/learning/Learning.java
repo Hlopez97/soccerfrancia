ce95739cb6115b22556d3a8686518b71f586cfa1
madwrist97 <maurojose1997@gmail.com>
2017-09-02 22:47:51 -0400

    Mensaje mostrado
    Cambie el nombre de la ventana

--- Learning/src/learning/Learning.java
+++ Learning/src/learning/Learning.java
@@ -36,7 +36,7 @@ public class Learning extends Application {
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Hola, soy una ventana y me llamo Natacha");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
